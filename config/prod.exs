6399d248f823361e8a4a44ca287295ee5cd1939c
Daniel McGrath <danmcgrath1035@gmail.com>
2021-02-27 01:17:05 -0500

    final change

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 
 config :bulls, BullsWeb.Endpoint,
-  url: [host: "hw05.danny-mcgrath.com", port: 80],
+  url: [host: "hw06.danny-mcgrath.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
